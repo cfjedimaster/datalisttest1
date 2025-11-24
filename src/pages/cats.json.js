9825c6d0009583f92cddb53b0b2e86eab0a59602
Raymond Camden <raymondcamden@gmail.com>
2025-11-24 17:20:18 -0600

    this will work 2

--- src/pages/cats.json.js
+++ src/pages/cats.json.js
@@ -3,30 +3,36 @@ const SITE = import.meta.env.WEBFLOW_SITE_ID;
 const KEY = import.meta.env.CMSKEY;
 const CATS = "691b3bd2bb9b164290a66b91";
 
-console.log("Fetching cats data 5");
-console.log("ensuring key works", KEY);
-console.log("trying to dump it all", SITE);
-console.log(JSON.stringify(import.meta.env));
+console.log("Fetching cats data 6");
+//console.log("ensuring key works", KEY);
+//console.log("trying to dump it all", SITE);
+//console.log(JSON.stringify(import.meta.env));
 
-const catsReq = await fetch(`https://api.webflow.com/v2/collections/${CATS}/items/live`, {
-  headers: {
-    "Authorization": `Bearer ${KEY}`,
-  }
-});
-const catsData = await catsReq.json();
-
-const cats = catsData.items.map(c => {
-  return {
-    ...c.fieldData,
-    id:c.id
-  }
-});
 
 export const config = {
     runtime: "edge",
 };
 
-export function GET({ params, request }) {
+export async function GET({ params, request, locals }) {
+
+  console.log('new fucking logic');
+  console.log(JSON.stringify(locals.runtime.env));
+  let KEY = locals.runtime.env.CMSKEY;
+  
+  const catsReq = await fetch(`https://api.webflow.com/v2/collections/${CATS}/items/live`, {
+    headers: {
+      "Authorization": `Bearer ${KEY}`,
+    }
+  });
+  const catsData = await catsReq.json();
+
+  const cats = catsData.items.map(c => {
+    return {
+      ...c.fieldData,
+      id:c.id
+    }
+  });
+
   return new Response(JSON.stringify(cats))
 }
 
