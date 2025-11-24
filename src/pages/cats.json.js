b3f1010f9c24bb30ef9836879ec8b43acd3e195b
Raymond Camden <raymondcamden@gmail.com>
2025-11-24 16:38:48 -0600

    even more testing

--- src/pages/cats.json.js
+++ src/pages/cats.json.js
@@ -3,8 +3,11 @@ const SITE = import.meta.env.WEBFLOW_SITE_ID;
 const KEY = import.meta.env.CMSKEY;
 const CATS = "691b3bd2bb9b164290a66b91";
 
-console.log("Fetching cats data 3");
+console.log("Fetching cats data 5");
 console.log("ensuring key works", KEY);
+console.log("trying to dump it all", SITE);
+console.log(JSON.stringify(import.meta.env));
+
 const catsReq = await fetch(`https://api.webflow.com/v2/collections/${CATS}/items/live`, {
   headers: {
     "Authorization": `Bearer ${KEY}`,
